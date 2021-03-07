d0a57c8275305b8d9224dd2d24226eceea5afa2a
Mason Kadem <39227979+masonkadem@users.noreply.github.com>
2021-03-07 10:56:05 -0500

    added neural net, gradient descent, and derivatives notebook

--- Do_gradient_descent.py
+++ Do_gradient_descent.py
@@ -6,3 +6,28 @@ Created on Thu Jan 28 14:03:45 2021
 @author: masonkadem
 """
 
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+def gradient_descent(X, y, learningrate = 0.05, epoch = 10):
+    
+    '''
+   Gradient descent math
+    '''
+    
+    m, b = 0.33, 0.48 # parameters
+    log, mse = [], [] # lists to store learning process
+    N = len(X) # number of samples
+    
+    for _ in range(epoch):
+                
+        f = y - (m*X + b)
+    
+        # Updating m and b
+        m -= lr * (-2 * X.dot(f).sum() / N)
+        b -= lr * (-2 * f.sum() / N)
+        
+        log.append((m, b))
+        mse.append(mean_squared_error(y, (m*X + b)))        
+    
+    return m, b, log, mse
